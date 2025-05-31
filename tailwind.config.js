8dd50af5ad1afca9bf109aaf05bb24d486859a4b
fseptianto <septianto.felix@gmail.com>
2025-05-31 14:41:40 +1000

    new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        accurment: ['TimesClassicDisplay', 'serif'],
+        lato: ['Lato', 'sans-serif'],
+      }
+    }
   },
   plugins: [],
 }
